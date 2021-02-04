109dcdc5976db73556c5e32c7d9d3a312bd8daa6
Tim Flood <tflood9@gmail.com>
2021-02-04 18:06:11 -0500

    functionality complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,46 +1,46 @@
 class ArticlesController < ApplicationController
-  before_action :set_course
-
-  def new
-    @article = @course.articles.build
+  def show
+    @article = Article.find(params[:id])
   end
 
-  def show
-    @article = @course.articles.find(params[:id])
+  def new
+    @course = Course.find(params[:course_id])
+    @article = @course.articles.build
   end
 
   def create
     article = Wikipedia.find(params[:article][:title])
 
+    @course = Course.find(params[:course_id])
     @article = @course.articles.build(
       title: article.title,
       iframe_link: article.fullurl
     )
 
-    if @article.save!
-      redirect_to edit_course_article_path(@course, @article)
+    if @article.save
+      redirect_to edit_article_path(@article)
     else
       render :new
     end
   end
 
   def edit
-    @article = @course.articles.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def update
-    @article = @course.articles.find(params[:id])
-    @article.update(article_params)
-    redirect_to course_path(@course)
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article.course
+    else
+      render :edit
+    end
   end
 
   private
 
-  def set_course
-    @course = Course.find(params[:course_id])
-  end
-
-  def article_params(*args)
-    params.require(:article).permit(*args)
+  def article_params
+    params.require(:article).permit(:intro, :outro)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :topics
   resources :courses do
-    resources :articles
+    resources :articles, only: [:new, :create]
   end
-
+  resources :articles, except: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
